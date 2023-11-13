195a687a8a58f97c3a903d3702ab8f3b2dbd5508
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-13 16:37:04 +0100

    Update AdvertisementRepository

--- src/main/java/com/example/marketplaceapi/repository/AdvertisementRepository.java
+++ src/main/java/com/example/marketplaceapi/repository/AdvertisementRepository.java
@@ -1,5 +1,6 @@
 package com.example.marketplaceapi.repository;
 
+import com.example.marketplaceapi.domain.dto.AdDTOView;
 import com.example.marketplaceapi.domain.entity.Advertisement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,10 +9,11 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Long> {
-    List<Advertisement> findByUser_UserId(Long userId);
+
     @Query("select a from Advertisement a where a.creationDate between :from and :to")
     List<Advertisement> selectAdvertisementBetweenDates(@Param("from") LocalDate from, @Param("to") LocalDate to);
 }

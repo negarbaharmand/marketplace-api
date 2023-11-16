8dba08483051a736f9acf0934bc1181243692a99
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-16 14:26:38 +0100

    Javadoc comments and optimise removeAd method

--- src/main/java/com/example/marketplaceapi/domain/entity/User.java
+++ src/main/java/com/example/marketplaceapi/domain/entity/User.java
@@ -4,9 +4,13 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * User Entity represents user table in the database
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,40 +19,74 @@ import java.util.List;
 @Entity
 @Table(name = "_user")
 public class User {
+    /**
+     * Unique identifier for a user.
+     */
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(name = "user_id", columnDefinition = "VARCHAR(255)")
     private String userId;
 
+    /**
+     * User's email address, must be unique.
+     */
     @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
     private String password;
 
-    private boolean expired = false;
-
+    /**
+     * List of advertisements associated with the user.
+     */
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Advertisement> advertisements = new ArrayList<>();
 
-
+    /**
+     * List of advertisements associated with the user.
+     */
     public User(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
+    /**
+     * Adds an advertisement to the user's list of advertisements.
+     *
+     * @param advertisement The advertisement to be added.
+     * @throws IllegalArgumentException If the provided advertisement is null.
+     */
     public void addAdvertisement(Advertisement advertisement) {
         if (advertisement == null) throw new IllegalArgumentException("Advertisement is null.");
+
+        if (advertisements == null) {
+            advertisements = new ArrayList<>();
+        }
+
         this.advertisements.add(advertisement);
+
         if (advertisement.getUser() != null) {
             advertisement.setUser(this);
         }
     }
 
-    public void removeAdvertisement(Advertisement advertisement) {
-        if (this.advertisements.remove(advertisement) && advertisement.getUser() == this) {
-            advertisement.setUser(null);
+    /**
+     * Removes expired advertisements from the user's list.
+     * An advertisement is considered expired if its expiration date is before the current date.
+     */
+    public void removeExpiredAdvertisements() {
+        List<Advertisement> advertisementsToRemove = new ArrayList<>();
+        LocalDate currentDate = LocalDate.now();
+
+        for (Advertisement ad : advertisements) {
+            if (ad.getExpirationDate().isBefore(currentDate)) {
+                advertisementsToRemove.add(ad);
+            }
         }
+
+        advertisements.removeAll(advertisementsToRemove);
+        advertisementsToRemove.forEach(advertisement -> advertisement.setUser(null));
     }
+
 }

7eb49f1d66bfad845d64b09e87cc7144c9545fe7
Negar Baharmand <negarbaharmand@outlook.com>
2023-11-16 15:20:07 +0100

    Optimise Repository layer's functionality and add Javadoc comments

--- src/main/java/com/example/marketplaceapi/repository/UserRepository.java
+++ src/main/java/com/example/marketplaceapi/repository/UserRepository.java
@@ -1,25 +1,32 @@
 package com.example.marketplaceapi.repository;
 
-import com.example.marketplaceapi.domain.dto.UserDTOView;
 import com.example.marketplaceapi.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Repository interface for managing User entities in the database.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
+    /**
+     * Checks if a user with the specified email exists.
+     *
+     * @param email The email to check.
+     * @return True if a user with the specified email exists, false otherwise.
+     */
     boolean existsByEmail(String email);
 
+    /**
+     * Retrieves an optional user by their email.
+     *
+     * @param email The email of the user.
+     * @return Optional containing the user with the specified email, or empty if not found.
+     */
     Optional<User> findByEmail(String email);
-
-    @Modifying
-    @Query("update User u set u.password = :password where u.email = :email")
-    void updatePasswordByEmail(@Param("email") String email, @Param("password") String newPassword);
 
 
 }
